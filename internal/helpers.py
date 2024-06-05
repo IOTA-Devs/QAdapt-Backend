16eff68b126a07e789119dc9f9268d414922eab1
Carlos Sandoval <cdavidsv6@gmail.com>
2024-06-04 20:10:18 -0600

    Implemented dashboard controller routes

--- internal/helpers.py
+++ internal/helpers.py
@@ -1,6 +1,41 @@
 import re
+from datetime import date, datetime
+from calendar import monthrange
+
+monthNamesAbbr = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec"
+}
 
 def sanitize_search_query(search_query: str):
     sanitized_query = re.sub(r'[^A-Za-z0-9 ]+', '', search_query)
 
     return sanitized_query
+
+def add_months(current_date: date, months: int):
+    years_to_add = (current_date.month + months) // 12
+    curr_month = (current_date.month + months) % 12
+    day = current_date.day
+
+    month_range = monthrange(current_date.year + years_to_add, curr_month)
+
+    if month_range[1] < day:
+        day -= month_range[1]
+
+    new_date = datetime(
+        current_date.year + years_to_add,
+        curr_month,
+        day
+    )
+
+    return new_date.date()
--- routers/api/dashboardController.py
+++ routers/api/dashboardController.py
@@ -1,10 +1,57 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, Query
+from datetime import datetime, date, timezone
+from pydantic import Field
 
 from ...middlewares import User, deserialize_user
+from ...internal import use_db, add_months, monthNamesAbbr
 
 router = APIRouter()
 
 @router.get("/general")
 async def get_general(current_user: Annotated[User, Depends(deserialize_user)]):
-    raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Not implemented")
+    with use_db() as (cur, _):
+        query = 'SELECT * FROM general_dashboard_data(%s)'
+        
+        cur.execute(query, [current_user.user_id])
+        general_data = cur.fetchall()
+
+        return {
+            "dashboard_data": general_data[0]
+        }
+
+@router.get("/tests_table_data")
+async def get_tests_table_data(
+    current_user: Annotated[User, Depends(deserialize_user)], 
+    start_date: date = Query(lt=datetime.now(tz=timezone.utc).date()),
+    end_date: date = Query(lte=datetime.now(tz=timezone.utc).date())):
+
+    with use_db() as (cur, _):
+        query = '''SELECT date_part('month', startTimestamp) AS month, COUNT(userId) FROM Tests 
+        WHERE userId = %s AND startTimestamp >= %s AND startTimestamp <= %s
+        GROUP BY date_part('month', startTimestamp)'''
+
+        cur.execute(query, [current_user.user_id, datetime.combine(start_date, datetime.min.time(), timezone.utc), datetime.combine(end_date, datetime.min.time(), timezone.utc)])
+        tests_per_month = cur.fetchall()
+
+        # populate the arrays
+        curr_date = start_date
+        graph_data = {}
+        for _ in range((start_date.year - end_date.year) * 12 + end_date.month - start_date.month + 1):
+            graph_data[monthNamesAbbr[curr_date.month] + "\n" + str(curr_date.year)] = 0
+            curr_date = add_months(curr_date, 1)
+        
+        for month_tests in tests_per_month:
+            graph_data[monthNamesAbbr[int(month_tests["month"])] + "\n" + str(datetime.now().year)] = month_tests["count"]
+        
+        labels = [] # labels is an array containing in this case the months of the year from the start_date to the end_date
+        data = [] # labels contains the data for each month in the labels array
+
+        labels, data = zip(*graph_data.items())
+
+        return {
+            "tests_graph_data": {
+                "labels": labels,
+                "data": data
+            }
+        }
